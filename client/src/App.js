4e3458a3279b5342f9d3b74558d207ae5f1cf3a4
Caleb <1calebtuttle@gmail.com>
2021-10-09 10:21:51 -0400

    add some code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import JobBoardContract from "./contracts/JobBoard.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  jobBoardState = {
+      contract: null
+  };
 
   componentDidMount = async () => {
     try {
@@ -23,8 +28,6 @@ class App extends Component {
         deployedNetwork && deployedNetwork.address,
       );
 
-      instance.options.address = "0xd9f7281aC8473B2dDe08448D6cDdf559EFB0C844";
-
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
@@ -34,6 +37,32 @@ class App extends Component {
         `Failed to load web3, accounts, or contract. Check console for details.`,
       );
       console.error(error);
+    }
+
+    
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = JobBoardContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        JobBoardContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
     }
   };
 
