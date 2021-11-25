00e923e02b1c5b16eb3772c08282f5d3ef99a6fd
Caleb <1calebtuttle@gmail.com>
2021-11-25 14:11:40 -0500

    Update README

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
 # ConsenSys Blockchain Developer Bootcamp Final Project
 
 ## Project Description
+My final project is a simple decentralized job board. It is a single page which lists jobs, allows users to add jobs, allows users to apply to jobs, and allows users to view applicants' addresses.
 
+The backend is one Solidity smart contract, and the frontend uses React.
 
 ## Directory Structure
+- client
+  - This contains the React frontend.
+- contracts
+  - This contains the JobBoard smart contract.
+- migrations
+  - This contains migration scripts.
+- test
+  - This contains the unit tests for the smart contract.
 
-## Where the Frontend can be Accessed
+## Installing Dependencies
+This project uses Truffle. The solc version is 0.8.0 (to be compatible with an OpenZeppelin library).
+
+React dependencies can be installed by running `npm install` the following command in the client/ directory.
+
+## Running Tests
+Tests are run on the local truffle develop testnet at port 8545. Use truffle to run the tests. Run this seqence of commands:
+
+    truffle develop
+    test
+
+## Accessing the Frontend
 where the frontend project can be accessed (see #8).
 
 ## My Ethereum Address (for the certificate NFT)
--- test/jobBoard.js
+++ test/jobBoard.js
@@ -1,6 +1,7 @@
 const JobBoard = artifacts.require("./JobBoard.sol");
 
 contract("JobBoard", accounts => {
+  // This test covers the postJob() and getJob() functions. 
   it("...should create a job post with the title 'Developer'.", async () => {
     const JobBoardInstance = await JobBoard.deployed();
     await JobBoardInstance.postJob("Developer", "desc", { from: accounts[0] });
@@ -8,6 +9,7 @@ contract("JobBoard", accounts => {
     assert.equal(jobPost.title, "Developer", "The job title is not 'Developer'");
   });
 
+  // This test covers the applyToJob() and viewApplicantAddresses() functions.
   it("...should apply to a job as the first applicant.", async () => {
     const JobBoardInstance = await JobBoard.deployed();
     await JobBoardInstance.applyToJob(1, "LinkedIn", { from: accounts[1] });
@@ -15,21 +17,22 @@ contract("JobBoard", accounts => {
     assert.equal(applicantAddresses[0], accounts[1], "The applicant address is not correct");
   });
 
+  // This test covers the applyToJob() function, checking that applicants are properly recorded
   it("...should apply to a job as the second applicant.", async () => {
     const JobBoardInstance = await JobBoard.deployed();
     await JobBoardInstance.applyToJob(1, "LinkedInURL", { from: accounts[2] });
     const applicantAddresses = await JobBoardInstance.viewApplicantAddresses.call(1);
     assert.equal(applicantAddresses[1], accounts[2], "The applicant address is not correct");
   });
-
-  it("...should delete the job with jobId 1.", async () => {
+  
+  // This test covers a successful call of the removeJob() function.
+  it("...should remove the job with jobId 1.", async () => {
     const JobBoardInstance = await JobBoard.deployed();
     const jobPost1 = await JobBoardInstance.getJob.call(1);
     await JobBoardInstance.removeJob(1);
     let success = false;
     try {
         const jobPost2 = await JobBoardInstance.getJob.call(1);
-
     } catch (error) {
         success = true;
     }
@@ -37,11 +40,12 @@ contract("JobBoard", accounts => {
     assert.equal(success, true, "Trying to view the removed job posting should return an error");
   });
 
+  // This test covers the postJob() and getJob() functions in more scenarios.
   it("...should create 3 jobs.", async () => {
     const JobBoardInstance = await JobBoard.deployed();
     await JobBoardInstance.postJob("Developer2", "desc", { from: accounts[0] });
-    await JobBoardInstance.postJob("Developer3", "desc", { from: accounts[0] });
-    await JobBoardInstance.postJob("Developer4", "desc", { from: accounts[0] });
+    await JobBoardInstance.postJob("Developer3", "desc", { from: accounts[1] });
+    await JobBoardInstance.postJob("Developer4", "desc", { from: accounts[2] });
     const jobPost2 = await JobBoardInstance.getJob.call(2);
     const jobPost3 = await JobBoardInstance.getJob.call(3);
     const jobPost4 = await JobBoardInstance.getJob.call(4);
@@ -50,6 +54,7 @@ contract("JobBoard", accounts => {
     assert.equal(jobPost4.title, "Developer4", "The job title is not 'Developer4'");
   });
   
+  // This test covers a failing call to the removeAllJobs() function.
   it("...should try to remove all 3 jobs (as a non-owner) but fail.", async () => {
     const JobBoardInstance = await JobBoard.deployed();
     let success = false;
@@ -61,6 +66,7 @@ contract("JobBoard", accounts => {
     assert.equal(success, true, "Non-owner address removed all jobs");
   });
 
+  // This test covers a successful call to the removeAllJobs() function.
   it("...should delete all 3 jobs.", async () => {
     const JobBoardInstance = await JobBoard.deployed();
     await JobBoardInstance.removeAllJobs({ from: accounts[0] });
