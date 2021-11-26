ca484f2e337d7a5ce1e8c635b9540110aaac6696
Caleb <1calebtuttle@gmail.com>
2021-11-26 11:14:20 -0500

    migrate contract to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,12 @@
 const path = require("path");
 
+// const HDWallet = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraURL = 'https://rinkeby.infura.io/v3/516556d62aab464d90a7eebe9d924aae';
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -7,6 +14,11 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infuraURL),
+      network_id: 4,  // Rinkeby's network id
+      gas: 5500000,
     }
   },
   compilers: {
